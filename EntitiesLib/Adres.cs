2af488f3df4c97efbce80542a6c01a6115016bd0
Geert Jansen <geert__jansen@hotmail.com>
2017-01-24 16:00:49 +0100

    Added different libs and Entity Framework NuGet Added Personen and Adres.

---
+++ EntitiesLib/Adres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLib
+{
+    public class Adres
+    {
+        public Adres()
+        {
+            Inwoners = new Personen();
+        }
+
+        public string Straatnaam { get; set; }
+        public string Stad { get; set; }
+
+        public Personen Inwoners { get; }
+    }
+}
---
+++ EntitiesLib/Personen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLib
+{
+    public class Personen
+    {
+        public Personen()
+        {
+            PersonenLijst = new List<Persoon>();
+        }
+
+        public List<Persoon> PersonenLijst { get; }
+    }
+}

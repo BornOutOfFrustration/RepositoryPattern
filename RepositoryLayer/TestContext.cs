2d9d05275b7dcbed2ddb8b5d2f54a6d816dbf5d6
Geert Jansen <geert__jansen@hotmail.com>
2017-01-25 13:02:15 +0100

    Added database connection and DbContext

---
+++ RepositoryLayer/TestContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EntitiesLib;
+
+namespace RepositoryLayer
+{
+    public class TestContext : DbContext
+    {
+        public DbSet<Persoon> Personen { get; set; }
+        public DbSet<Adres> Adressen { get; set; }
+    }
+}

c88883d3626393870a8440febfb047c801097ec6
Geert Jansen <geert__jansen@hotmail.com>
2017-01-26 09:07:23 +0100

    Entered some test code to initialize the database tables

--- RepositoryPattern/Program.cs
+++ RepositoryPattern/Program.cs
@@ -10,6 +10,18 @@ namespace RepositoryPattern
     {
         static void Main(string[] args)
         {
+            RepositoryLayer.TestContext context = new RepositoryLayer.TestContext();
+            var p1 = new EntitiesLib.Persoon() { Achternaam = "geert", Voornaam = "jansen" };
+            var p2 = new EntitiesLib.Persoon() { Achternaam = "person", Voornaam = "unknown" };
+
+            context.Personen.Add(p1);
+            context.Personen.Add(p2);
+
+            var a1 = new EntitiesLib.Adres() { Stad = "rotterdam", Straatnaam = "mijn straat" };
+            a1.Inwoners.PersonenLijst.Add(p1);
+            a1.Inwoners.PersonenLijst.Add(p2);
+            context.Adressen.Add(a1);
+            context.SaveChanges();
         }
     }
 }
